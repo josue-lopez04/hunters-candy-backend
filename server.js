09ea7fddc9a7b0353bd455371d134f133f00831e
jos <jos@lap-23.local>
2025-04-01 18:20:50 -0600

    cambios socket

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
 
 // Configuración CORS mejorada
 app.use(cors({
-  origin: function(origin, callback) {
+/*   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
     
     if (allowedOrigins.indexOf(origin) !== -1) {
@@ -36,7 +36,8 @@ app.use(cors({
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  },
+  }, */
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
